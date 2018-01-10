1b76590f310be58d9120b211737490f1f5a5a3f6
Ivo Georgiev <ivo.pl.georgiev@gmail.com>
2018-01-11 01:00:59 +0200

    #9 remove redundant code from fcn

--- tools/ukfCfgGen/mtx2carr.m
+++ tools/ukfCfgGen/mtx2carr.m
@@ -8,11 +8,11 @@ function str = mtx2carr(mtx)
 str = arrayfun(@num2str, mtx, 'UniformOutput', false);
 if c > 1    
     str = cellfun(@strcat,str, repmat({','},r,c),'UniformOutput',false);
-    str = [cellfun(@strcat,repmat({'{'},r,1),str(:,1),'UniformOutput',false) str(:,2:end)];
-    str = [str(:,1:end-1) cellfun(@strcat,str(:,end),repmat({'},'},r,1),'UniformOutput',false)];
+    str(:,1) = cellfun(@strcat,repmat({'{'},r,1),str(:,1),'UniformOutput',false);
+    str(:,end) = cellfun(@strcat,str(:,end),repmat({'},'},r,1),'UniformOutput',false);
 else
-    str = [cellfun(@strcat,repmat({'{'},r,1),str(:,1),'UniformOutput',false) str(:,2:end)];
-    str = [str(:,1:end-1) cellfun(@strcat,str(:,end),repmat({'},'},r,1),'UniformOutput',false)];
+    str(:,1) = cellfun(@strcat,repmat({'{'},r,1),str(:,1),'UniformOutput',false);
+    str(:,end) = cellfun(@strcat,str(:,end),repmat({'},'},r,1),'UniformOutput',false);
 end;
 str = num2cell(str,1);
 str = strcat(str{:});
