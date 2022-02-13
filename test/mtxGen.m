749c52bd8a399cd1795bf9061c9eed064cc06bb7
Iso <ivo.pl.georgiev@gmail.com>
2022-02-13 18:43:21 +0200

    Update mtxGen.m

--- test/mtxGen.m
+++ test/mtxGen.m
@@ -18,7 +18,27 @@ for i=2:max_size %matrix size i x i
         mtx_cell(j+(i-2)*max_num+(k-1)+(c-1),:) = {mtx2arr(mtx,mtx_name)};
         mtx_cell(j+(i-2)*max_num+k+(c-1),:) = {mtx2arr(mtx_inv,mtx_name_inv)};
         mtx_cell(j+(i-2)*max_num+k+c,:) = {mtx2arr(mtx_chol,mtx_name_chol)};
-
+        offset = j+(i-2)*max_num+k+c;
+        k=k+1;
+        c=c+1;
+    end
+end
+%%%
+k=1;c=1;
+for i=2:max_size %matrix size i x i
+    for j = 1:max_num %matrix number
+        mtx_name = ['mtx_' num2str(j) '_' num2str(i) 'x' num2str(i)];
+        mtx_name_inv = [mtx_name '_inv'];
+        mtx_name_chol = [mtx_name '_chol'];
+        
+        obj_name = ['obj_' num2str(j) '_' num2str(i) 'x' num2str(i)];
+        obj_name_inv = [obj_name '_inv'];
+        obj_name_chol = [obj_name '_chol'];
+            
+        mtx_cell(offset+j+(i-2)*max_num+(k-1)+(c-1),:) = {['Matrix_t ' obj_name '={' num2str(i*i) ',' num2str(i) ',' num2str(i) ',&' mtx_name '[0][0]};'; ]};
+        mtx_cell(offset+j+(i-2)*max_num+k+(c-1),:) = {['Matrix_t ' obj_name_inv '={' num2str(i*i) ',' num2str(i) ',' num2str(i) ',&' mtx_name_inv '[0][0]};'; ]};
+        mtx_cell(offset+j+(i-2)*max_num+k+c,:) = {['Matrix_t ' obj_name_chol '={' num2str(i*i) ',' num2str(i) ',' num2str(i) ',&' mtx_name_chol '[0][0]};'; ]};
+        
         k=k+1;
         c=c+1;
     end
